65114c3a1bf8a08ebdd6781462a9a92c5c03558d
Jordan Porter <jordan@mtnra.com>
2019-12-02 10:29:39 -0800

    push build

---
+++ docs/precache-manifest.ea3a8878e9a5df29d2f4de569236f6f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dacab8ca79e46e8df83ab603e1e2e63",
+    "url": "/nike/index.html"
+  },
+  {
+    "revision": "c637f8846c035bda6c7b",
+    "url": "/nike/static/css/main.5c2de3ee.chunk.css"
+  },
+  {
+    "revision": "32b746de586823cf4465",
+    "url": "/nike/static/js/2.7e27c4d5.chunk.js"
+  },
+  {
+    "revision": "c637f8846c035bda6c7b",
+    "url": "/nike/static/js/main.121d0308.chunk.js"
+  },
+  {
+    "revision": "96d09d2f40222c9b649d",
+    "url": "/nike/static/js/runtime-main.7331537d.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.42ec3cb2d389a547f3c3c876c7e9d2d9.js"
+  "/nike/precache-manifest.ea3a8878e9a5df29d2f4de569236f6f5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/nike/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
