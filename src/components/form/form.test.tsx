7e90515501e731eee7743d09f9bd7796e5717863
Jordan Porter <jordan@mtnra.com>
2019-12-02 10:44:04 -0800

    added simple no-crash tests

--- src/components/form/form.test.tsx
+++ src/components/form/form.test.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Component from './form';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import appReducer from '../../reducers/app-reducer';
+
+const store: any = createStore(appReducer);
 
 const f = jest.fn;
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Component open={true} setOpen={f} setSnackBar={f} />, div);
+  ReactDOM.render(
+    <Provider store={store}>
+      <Component open={true} setOpen={f} setSnackBar={f} />
+    </Provider>,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
