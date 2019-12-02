6c45785a4f751aca3c98047f99542b8e10c655fc
Jordan Porter <jordan@mtnra.com>
2019-12-01 23:48:48 -0800

    added build

---
+++ build/precache-manifest.42ec3cb2d389a547f3c3c876c7e9d2d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cbc04ec59107096d5e7b58100478bae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc09c97f4d2fb4499c7a",
+    "url": "/static/css/main.5c2de3ee.chunk.css"
+  },
+  {
+    "revision": "32b746de586823cf4465",
+    "url": "/static/js/2.7e27c4d5.chunk.js"
+  },
+  {
+    "revision": "fc09c97f4d2fb4499c7a",
+    "url": "/static/js/main.c8561fb3.chunk.js"
+  },
+  {
+    "revision": "b8c8f31249b397b3186d",
+    "url": "/static/js/runtime-main.0a7fa851.js"
+  }
+]);
